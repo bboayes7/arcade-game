cc1b1eb630df8e430e64b8950e765f6663ed3ca4
Barry <barryboayes17@gmail.com>
2019-02-23 06:19:34 -0800

    finished

--- README.md
+++ README.md
@@ -1,20 +1,17 @@
-# Classic Arcade Game Clone Project
+# Arcade Game Project
 
-## Table of Contents
+## Description
 
-- [Instructions](#instructions)
-- [Contributing](#contributing)
+Simple Arcade Game of Frogger used with Udacity's Starter Code. The game begins when the page starts up.
 
-## Instructions
+## How to Run
+To get started run the index.html on your browser and start playing.
 
-Use this [rubric](https://review.udacity.com/#!/rubrics/15/view) for self-checking your submission.
+## How To Play
+Use your arrow keys to move your player.
+To win:
+    - Avoid all bugs on your way to the water
+    - When you reach the water, you win
 
-Make sure the functions you write are **object-oriented** - either class functions (like `Player` and `Enemy`) or class prototype functions such as `Enemy.prototype.checkCollisions`. Also make sure that the keyword `this` is used appropriately within your class and class prototype functions to refer to the object the function is called upon.
-
-Your **README.md** file should be updated with instructions on both how to 1. Run and 2. Play your arcade game.
-
-For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
-
-## Contributing
-
-This repository is the starter code for _all_ Udacity students. Therefore, we most likely will not accept pull requests.
+To Lose:
+    - Get hit by any bug
--- js/app.js
+++ js/app.js
@@ -1,39 +1,119 @@
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+//Enemy Class
+class Enemy {
+    //Enemy Constructor
+    //x, y, and speed properties are randomized using this randomizer method
+    constructor(){
+        this.sprite = 'images/enemy-bug.png';
+        this.randomizer();
+    }
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-};
+    //update method
+    update(dt){
+        //increments the x-movement by the enemy's speed times dt
+        this.x += this.speed * dt;
+        
+        //if enemy moves fully across the screen, reset it's attributes randomly
+        if(this.x > 606){
+            this.randomizer();
+        }
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
+        //if the enemy collides with the player, set the player's coordinates to the initial coordinates
+        if(collide(player, this)){
+            winLoseMessage(false);
+            player.x = 200;
+            player.y = 380;
+        }
+    }
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+    render(){
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+    //randomizes the enemy's attributes
+    randomizer(){
+        //only 3 sets of Y coordinates the enemy is allowed on
+        const setYs = [214, 131, 48];
+
+        //have the enemy start off screen
+        this.x = -101
+
+        //randomize which y coordinate it starts from
+        this.y = setYs[Math.floor(Math.random() * setYs.length)];
+
+        //randomize the speed of the enemy from 50 - 300;
+        this.speed = Math.random() * (300 - 50) + 50;
+    }
+
+}
+
+//Player class
+class Player {
+    //have player's set coordinates to (200, 380) 
+    constructor(){
+        this.x = 200;
+        this.y = 380;
+        this.sprite = 'images/char-boy.png';
+    }
+
+    //update function (i really dont know what to do with this, the game runs fine without it)
+    update(){
+        this.x = this.x;
+        this.y = this.y;
+    }
 
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+    render(){
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
 
+    //handle input function takes an input string provided by the event listener
+    handleInput(input){
+        //use a switch to handle the player's coordinates according to the input.
+        //if the coordinate is out of bounds, then keep adding the amount back to the coordinate
+        //so the player doesn't move.
+        switch(input){
+            case 'left':
+                this.x -= 101;
+                if(this.x === -103)
+                    this.x += 101;
+                break;
+            case 'right':
+                this.x += 101;
+                if(this.x === 503)
+                    this.x -= 101;
+                break;
+            case 'up':
+                this.y -= 83;
+                //if the y coordinate is at -35, the player reaches the water and wins the game.
+                //set the coordinates back to the original coordinates
+                if(this.y === -35){
+                    winLoseMessage(true);
+                    this.x = 200;
+                    this.y = 380;
+                }
+                break;
+            case 'down':
+                this.y += 83;
+                if(this.y === 463)
+                    this.y -= 83;
+                break;
+        }
+    }
+}
 
+//instantiate objects with the recommended names
+const allEnemies = [new Enemy(), new Enemy(), new Enemy(), new Enemy(), new Enemy()];
+const player = new Player();
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+//collide function
+//logic borrowed from http://blog.sklambert.com/html5-canvas-game-2d-collision-detection/#d-collision-detection
+function collide(player, enemy){
+    if(player.x < enemy.x + 65 && player.x + 65 > enemy.x &&
+        player.y < enemy.y + 50 && player.y + 50 > enemy.y){
+        return true;
+    }
+}
+
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
@@ -44,3 +124,25 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
+//Send a quick message to the player when he/she wins or gets hit
+function winLoseMessage(win){
+    //create an h1
+    const h1 = document.createElement('h1');
+    //if you win, send a message in green
+    //if you lose, send a message in red
+    if(win){
+        h1.textContent = 'Congrats you win!';
+        h1.style.color = '#00FF7F';
+    } else {
+
+        h1.textContent = 'Ouch! Try Again.';
+        h1.style.color = '#ee1111';
+    }
+
+    //append the h1 to the body
+    document.body.appendChild(h1);
+
+    //remove the h1 so the player can continue the game
+    setTimeout(() =>document.querySelector('h1').remove(), 1000);   
+}
